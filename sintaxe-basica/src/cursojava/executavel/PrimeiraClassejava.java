82665707dbccd86ac0d3049ca919c6ba3d69c9e6
Gabriel Thiago Da Silva <79329058+Gabrielxdev@users.noreply.github.com>
2024-01-22 19:27:25 -0300

    criação do projeto, comitando o projeto
    criação do projeto

--- sintaxe-basica/src/cursojava/executavel/PrimeiraClassejava.java
+++ sintaxe-basica/src/cursojava/executavel/PrimeiraClassejava.java
@@ -4,6 +4,7 @@ public class PrimeiraClassejava {
 	/*main é um metedo auto executavel em java*/
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em java");
+		System.out.println("Olá, testando");
 		
 	}
 
